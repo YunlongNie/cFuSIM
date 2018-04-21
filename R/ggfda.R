43e448d9345c26dd68e1507721e0f42309c5302a
YunlongNie <yunlong.nie@yahoo.ca>
2018-04-20 22:28:04 -0700

    add example

--- R/ggfda.R
+++ R/ggfda.R
@@ -1,3 +1,17 @@
+#' This function computes the coeffcients of the index function 
+#' @param fdobj fda object 
+#' @export fdagg 
+#' @import fda
+#' @import dplyr
+#' @import ggplot
+#' @examples
+#' \dontrun{
+#' library(fda)
+#' library(cFuSIM)
+#' spline.basis=create.bspline.basis(rangeval=c(1,24),nbasis=10,norder)
+#' beta_fd = fd(rnorm(10), spline.basis)
+#' plot(beta_fd)
+#' }
 fdagg = function(fdobj, ylab='Value', xlab="Time", addzero = FALSE)
 {
 range= fdobj$basis$rangeval
@@ -13,5 +27,6 @@ plotdata = reshape2::melt(data, id=1)
 
 plot = ggplot2::ggplot(plotdata,aes(x=Time, y=value,group=factor(variable), color=factor(variable)))+geom_line()+theme_bw()+ylab(ylab)+xlab(xlab)
 if (addzero) plot = plot+geom_hline(yintercept=0, linetype=2, col=2)
+plot = plot+theme(legend.position = "none")
 return(plot)
 }
