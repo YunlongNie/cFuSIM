f0e9d4b8a12fdfa992978a59152380f718e8d228
YunlongNie <yunlong.nie@stat.ubc.ca>
2021-02-01 09:46:34 -0800

    Update pmatf_locploy.R

--- R/pmatf_locploy.R
+++ R/pmatf_locploy.R
@@ -3,17 +3,20 @@
 #' @import NonpModelCheck
 
 pmatf_locploy = function(betac ,ytrain=ytrain,xtrain=xtrain, spline_basis){
-B = inprod(spline_basis,spline_basis,0,0)
-pc_fit = fd(betac,spline_basis)
-alpha = inprod(xtrain,spline_basis)
-score_fit = inprod(xtrain,pc_fit)%>%as.numeric
-loc_fit = localpoly.reg(score_fit, ytrain, degree.pol = 1, kernel.type = "gaussian",bandwidth = "CV",deriv=0)
-fity = loc_fit$predicted
-loc_fit = localpoly.reg(score_fit, ytrain, degree.pol = 1, kernel.type = "gaussian",bandwidth = "CV",deriv=1)
-derfity = loc_fit$predicted
-cc = ytrain - fity-derfity
-Ba = B%*%t(alpha)
-mattemp = matrix(rep(cc, each=spline_basis$nbasis), nrow=spline_basis$nbasis)*Ba
-Pmat = mattemp%*%t(mattemp)
-Pmat 
+  B = inprod(spline_basis, spline_basis, 0, 0)
+  pc_fit = fd(betac, spline_basis)
+  Z = inprod(xtrain, spline_basis)
+  score_fit = inprod(xtrain, pc_fit) %>% as.numeric
+  bandwidth0 = bandwidth
+  loc_fit = localpoly.reg(score_fit, ytrain, degree.pol = 1,
+                          kernel.type = "gaussian", bandwidth = bandwidth0 , deriv = 0)
+  select_bandwith = loc_fit$bandwidth
+  fity = loc_fit$predicted
+  loc_fit = localpoly.reg(score_fit, ytrain, degree.pol = 1,
+                          kernel.type = "gaussian", bandwidth = loc_fit$bandwidth, deriv = 1)
+  derfity = loc_fit$predicted
+  
+  Gmat = list()
+  yweighted = ytrain - fity + derfity*(Z%*%betac)
+  list( bandwidth = select_bandwith, weights = -derfity, yweighted = yweighted, Z = Z)
 }
